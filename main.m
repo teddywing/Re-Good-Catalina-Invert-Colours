2c603a6077dad666ea89b0f3fcf3581676575f53
Teddy Wing <git@teddywing.com>
2021-03-04 20:02:32 +0100

    Skeleton for error handling

--- main.m
+++ main.m
@@ -3,13 +3,17 @@
 int main(int argc, const char * argv[]) {
     const CGGammaValue gamma[2] = {1, 0};
 
-    CGSetDisplayTransferByTable(
+    CGError error = CGSetDisplayTransferByTable(
         CGMainDisplayID(),
         2,
         gamma,
         gamma,
         gamma
     );
+
+    if (error != kCGErrorSuccess) {
+        // TODO: error handling
+    }
 
     for (;;) {}
 
