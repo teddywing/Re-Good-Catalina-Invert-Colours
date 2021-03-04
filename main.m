aea4d2b64d3a0017c4a68b03acb5bbd063a08ab9
Teddy Wing <git@teddywing.com>
2021-03-04 20:44:59 +0100

    Invert all displays
    Previously, I was only inverting the main display. Get a list of active
    displays and invert all of them.
    
    Decided to set the maximum display count to eight because that seemed
    like a sufficiently high number for most people.

--- main.m
+++ main.m
@@ -1,18 +1,33 @@
 #import <CoreGraphics/CoreGraphics.h>
 
+#define MAX_DISPLAYS 8
+
 int main(int argc, const char * argv[]) {
     const CGGammaValue inverted_gamma[2] = {1, 0};
 
-    CGError error = CGSetDisplayTransferByTable(
-        CGMainDisplayID(),
-        2,
-        inverted_gamma,
-        inverted_gamma,
-        inverted_gamma
-    );
+    CGDirectDisplayID active_displays[MAX_DISPLAYS];
+    uint32_t display_count;
 
+    CGError error = CGGetActiveDisplayList(
+        MAX_DISPLAYS,
+        &active_displays[0],
+        &display_count
+    );
     if (error != kCGErrorSuccess) {
-        // TODO: error handling
+        return 69;
+    }
+
+    for (int i = 0; i < display_count; i++) {
+        error = CGSetDisplayTransferByTable(
+            active_displays[i],
+            2,
+            inverted_gamma,
+            inverted_gamma,
+            inverted_gamma
+        );
+        if (error != kCGErrorSuccess) {
+            // TODO: error handling
+        }
     }
 
     for (;;) {}
