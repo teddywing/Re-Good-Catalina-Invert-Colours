4a60b69cefa5d9053dc33f667ad908a68590a54c
Teddy Wing <git@teddywing.com>
2021-03-07 16:26:59 +0100

    Add license (GNU GPLv3+)

--- main.m
+++ main.m
@@ -1,3 +1,21 @@
+// Copyright (c) 2021  Teddy Wing
+//
+// This file is part of Re-Good Catalina Invert Colours.
+//
+// Re-Good Catalina Invert Colours is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// Re-Good Catalina Invert Colours is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+// the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Re-Good Catalina Invert Colours. If not, see
+// <https://www.gnu.org/licenses/>.
+
 #import <Carbon/Carbon.h>
 
 #include <sysexits.h>
